77a9e07fd50ca946d6ec29a727ae99110dc55084
Irene Zanardi <irezan@users.noreply.github.com>
2020-07-20 17:17:32 +0200

    ColoreGrabberFix

--- Assets/02_SCRIPTS/Arduino/ColorGrabber.cs
+++ Assets/02_SCRIPTS/Arduino/ColorGrabber.cs
@@ -8,13 +8,12 @@ public class ColorGrabber : MonoBehaviour {
     private float greenComponent;
 
     void Start(){
-        
+
         float blueGreenComponent = 0.5647059f;
         float greenGreenComponent = 0.7764706f;
-        float yellowGreenComponent = 0.6821074f;
+        float yellowGreenComponent = 0.6821074f; 
         float orangeGreenComponent = 0.439802f;
         float redGreenComponent = 0.39102f;
-
 
         List<MeshRenderer> rendererList = new List<MeshRenderer>();
         GetComponentsInChildren<MeshRenderer>(false, rendererList);
@@ -22,27 +21,31 @@ public class ColorGrabber : MonoBehaviour {
         if (rendererList != null){
                         
             foreach (MeshRenderer rendererPart in rendererList){
+                
                 greenComponent = rendererPart.material.GetColor("_Color").g;
-                //Debug.Log("elenco lista " + rendererPart.ToString() + "colore verde: " + greenComponent);
+                //Debug.Log("elenco lista: " + rendererPart.ToString() + " colore: " + greenComponent);
 
-               if (greenComponent == blueGreenComponent){
+                if (Mathf.Approximately(greenComponent, blueGreenComponent)){
                     arduinoColor = "b";
-                    Debug.Log("Ho trovato un oggetto blu");
+                    Debug.Log("QUI!!!! Ho trovato un oggetto blu");
                     break;
-                 } else if (greenComponent == greenGreenComponent){
+                 } else if (Mathf.Approximately(greenComponent, greenGreenComponent)){
                     arduinoColor = "g";
+                    Debug.Log("QUI!!!! Ho trovato un oggetto verde");
                     break;
-                } else if (greenComponent == yellowGreenComponent){
+                } else if (Mathf.Approximately(greenComponent, yellowGreenComponent)){
                     arduinoColor = "y";
+                    Debug.Log("QUI!!!! Ho trovato un oggetto giallo");
                     break;
-                } else if (greenComponent == orangeGreenComponent){
+                } else if (Mathf.Approximately(greenComponent, orangeGreenComponent)){
                     arduinoColor = "w";
+                    Debug.Log("QUI!!!! Ho trovato un oggetto arancio");
                     break;
-                } else if (greenComponent == redGreenComponent){
+                } else if (Mathf.Approximately(greenComponent, redGreenComponent)){
                     arduinoColor = "r";
+                    Debug.Log("QUI!!!! Ho trovato un oggetto rosso");
                     break;
                 }
-                 
             }
 
         }
