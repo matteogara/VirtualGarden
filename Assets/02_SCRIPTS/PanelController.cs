22dcf7da1a4627dfdeb85b63376c095fc6027d06
Irene Zanardi <irezan@users.noreply.github.com>
2020-07-22 00:32:11 +0200

    WorldSpace UI

--- Assets/02_SCRIPTS/PanelController.cs
+++ Assets/02_SCRIPTS/PanelController.cs
@@ -6,19 +6,18 @@ public class PanelController : MonoBehaviour{
 
     private Vector3 panelDestination;
 
-    private GameObject panelObj;
+    public GameObject panelObj;
 
     //questo potrebbe essere il valore della rotazione del controller, al momento lo modifico dall'inspector per debuggare
     public Quaternion controllerRotation;
    
     void Start(){
-        panelObj = GameObject.Find("Panel");
+        
     }
 
     void Update(){
         panelDestination = GameObject.Find("panelDestination").transform.position;
-        Debug.Log("Panel destination: " + panelDestination);
-
+        
         //controllerRotation = GameObject.Find("NOME CONTROLLER").transform.rotation;
         //Debug.Log("Panel rotation: " + controllerRotation);
 
